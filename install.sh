806f95c7667e4cec9db285245d99694b0d46aeb3
froginwell <1508257804@qq.com>
2017-07-14 22:18:41 +0800

    add dependency

--- install.sh
+++ install.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+
+# install vim
+sudo apt-get install -y python-dev python3-dev libncurses5-dev
+git clone https://github.com/vim/vim.git
+cd vim
+./configure --with-features=huge --enable-python3interp --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/ --enable-rubyinterp --enable-luainterp --enable-perlinterp --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu/ --enable-multibyte --enable-cscope --prefix=/usr/local/vim/
+make
+sudo make install
+sudo ln -s /usr/local/vim/bin/vim /usr/local/bin/vim
+
+# vim config
 if [ ! -d ~/.vim ]
 then
     mkdir ~/.vim
 fi
 
-# vim config
 find . -type f | egrep -v ".git|.gitignore|install.sh|myvim.png|README.md" | cpio -admvp ~/.vim
 
 # install youcompleteme
+sudo --set-home pip install requests-future
 sudo apt-get install -y vim-youcompleteme
 sudo apt-get install -y vam-addon-manager
 vam install youcompleteme
